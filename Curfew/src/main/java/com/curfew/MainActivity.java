b4bf17d133cd3a931f9d2e5ea388d9b55c710c2f
Rohan Shah <rohan8@gmail.com>
2013-10-30 18:01:09 -0500

    basic curfew-setting functionality. In the Parse datastore you can see the established curfew with the from user and to user.

--- Curfew/src/main/java/com/curfew/MainActivity.java
+++ Curfew/src/main/java/com/curfew/MainActivity.java
@@ -2,16 +2,28 @@ package com.curfew;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     private TextView mUserNameTextView;
+    private TextView mSetCurfewTextView;
+    private TimePicker timePicker;
+    private String TAG = "com.curfew.MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +34,57 @@ public class MainActivity extends Activity {
 
         //Get username textview
         mUserNameTextView = (TextView) findViewById(R.id.userNameTextView);
+
         ParseUser currentUser = ParseUser.getCurrentUser();
         if(currentUser != null)
             mUserNameTextView.setText(currentUser.getUsername());
         else{
             //do nothing for now
         }
+        findViewById(R.id.setCurfewButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSetCurfewTextView = (TextView) findViewById(R.id.editText);
+                timePicker = (TimePicker) findViewById(R.id.timePicker);
+                createCurfew();
+            }
+        });
 
-//        currentUser.
+
+
     }
+    public void createCurfew(){
+        ParseObject newCurfew= new ParseObject("Curfew");
+        ParseUser currentUser = ParseUser.getCurrentUser();
 
+        ParseQuery parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereEqualTo("username", mSetCurfewTextView.getText());
+        ParseRelation fromUserRelation = newCurfew.getRelation("FromUser");
+        fromUserRelation.add(currentUser);
+        //newCurfew.put("FromUser", fromUserRelation);
+        try{
+            List<ParseUser> userList = parseQuery.find();
+            if (userList.size() != 1){
+                Log.d(TAG, "User " + mSetCurfewTextView.getText() + " found " + userList.size() + " copies of the user");}
+            else{
+                ParseRelation toUserRelation = newCurfew.getRelation("ToUser");
+                toUserRelation.add(userList.get(0));
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+                String dateTime = "" + hour + ":" + minute;
+                newCurfew.put("Curfew", dateTime);
+                newCurfew.save();
+            }
+        }catch(ParseException e){
+           Log.d(TAG, "ParseException thrown when finding user: ", e);
+        }
+
+
+
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
